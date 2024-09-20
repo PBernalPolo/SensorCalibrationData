da8e667f6ca6d907fcc7adfaa8cad7cae6ecea9c
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-09-20 19:59:16 +0200

    fix: arduinoMegaGnssDataLogger
    The sketch was not working properly. The problem was identified to be the Serial buffers getting full. The sketch has been optimized to minimize the probability of this to happen, and by increasing the SERIAL_RX_BUFFER_SIZE. This needs to be done manually, so a comment has been included in the sketch description.

--- src/main/arduino/arduinoMegaGnssDataLogger/arduinoMegaGnssDataLogger.ino
+++ src/main/arduino/arduinoMegaGnssDataLogger/arduinoMegaGnssDataLogger.ino
@@ -1,6 +1,13 @@
 /*
   GNSS data logger using an Arduino MEGA2560 and 3 NEO-6M GPS.
 
+  This sketch only worked after increasing the buffer size in:
+  /home/<user>/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/HardwareSerial.h
+  by modifying the line:
+  #define SERIAL_RX_BUFFER_SIZE 64
+  to
+  #define SERIAL_RX_BUFFER_SIZE 512 //64
+
   Connections:
     SD card attached to SPI bus as follows:
     * GND - GND
@@ -21,6 +28,16 @@
 
 
 const int chipSelect = 53;  // for MEGA2560
+const int bufferSize = 1024;
+#define SERIAL_AVAILABLE_THRESHOLD 16
+
+uint8_t buffer1[bufferSize];
+uint8_t buffer2[bufferSize];
+uint8_t buffer3[bufferSize];
+
+int bufferIndex1;
+int bufferIndex2;
+int bufferIndex3;
 
 
 void setup()
@@ -52,50 +69,68 @@ void setup()
   // Initialize SD card.
   Serial.print("Initializing SD card...");
   // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+  if( !SD.begin( SPI_FULL_SPEED , chipSelect ) ) {
+    Serial.println( " card failed, or not present." );
     // don't do anything more:
     while (1);
   }
-  Serial.println("card initialized.");
+  Serial.println(" card initialized.");
 
-  // If we reached this point, turn the LED off to indicate the user that the setup function is complete, and we are going into the loop.
+  bufferIndex1 = 0;
+  bufferIndex2 = 0;
+  bufferIndex3 = 0;
+
+  // LED off when setup is complete, and we are going into the loop.
   digitalWrite( LED_BUILTIN , LOW );
 }
 
 
 void loop()
 {
-  // Write data of each serial to a file.
-  writeGnssDataToSdCard( Serial1 , "datalog1.txt" );
-  writeGnssDataToSdCard( Serial2 , "datalog2.txt" );
-  writeGnssDataToSdCard( Serial3 , "datalog3.txt" );
-
+  if( Serial1.available() > 0 ) {
+    collectGnssData( Serial1 , buffer1 , bufferIndex1 );
+  } else if( Serial2.available() > 0 ) {
+    collectGnssData( Serial2 , buffer2 , bufferIndex2 );
+  } else if( Serial3.available() > 0 ) {
+    collectGnssData( Serial3 , buffer3 , bufferIndex3 );
+  } else {
+    writeBufferToSd( buffer1 , bufferIndex1 , "datalog1.txt" );
+    writeBufferToSd( buffer2 , bufferIndex2 , "datalog2.txt" );
+    writeBufferToSd( buffer3 , bufferIndex3 , "datalog3.txt" );
+  }
 }
 
 
-/**
- * Writes GNSS data received by serialPort to a file.
- */
-void writeGnssDataToSdCard( Stream& serialPort , String filename ) {
-  // If there is available data,
-  if( serialPort.available() ) {
-    // try to open the file.
-    File dataFile = SD.open( filename , FILE_WRITE );
-    // Return if we fail to open the file.
-    if( !dataFile ) {
-      Serial.println( "Failed to open" + filename );
-      return;
+void collectGnssData( Stream& serialPort , uint8_t* buffer , int& bufferIndex )
+{
+  buffer[bufferIndex++] = serialPort.read();
+  //int numberOfBytesRead = serialPort.readBytes( &buffer[bufferIndex] , serialPort.available() );
+  //bufferIndex += numberOfBytesRead;
+}
+
+
+void writeBufferToSd( uint8_t* buffer , int& bufferIndex , String filename )
+{
+  if( bufferIndex > 0 ) {
+    File dataFile = SD.open( filename , O_WRITE | O_CREAT | O_APPEND );
+    if( dataFile ) {
+      if( Serial ) {
+        Serial.write( "Writting to " );
+        Serial.print( filename );
+        Serial.write( "..." );
+      }
+      dataFile.write( buffer , bufferIndex );
+      dataFile.close();
+      bufferIndex = 0;
+      if( Serial ) {
+        Serial.write( " done.\n" );
+      }
+    } else {
+      if( Serial ) {
+        Serial.write( "Error opening " );
+        Serial.print( filename );
+        Serial.write( "\n" );
+      }
     }
-    // Send a message through the serial port indicating that we are writting to file.
-    Serial.print( "Writting to " + filename + "... " );
-    // Write available data to file.
-    while( serialPort.available() ) {
-      dataFile.print( char(serialPort.read()) );
-    }
-    // Finally, close the file,
-    dataFile.close();
-    // and send a message through the serial port saying that we wrote the data.
-    Serial.println( "done." );
   }
 }
